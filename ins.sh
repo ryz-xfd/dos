fba092dfc35e380f908e39b5ab4634a545fbc164
ryz. <xcvxddvbghjng@gmail.com>
2024-09-12 14:12:28 +0700

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -1,1 +1,1 @@
-apt install sudo && sudo apt install python && sudo apt install git && git clone https://github.com/xShamir/DDoS-Layer4 dos && cd dos && python3 DDoS.py
+apt install sudo && sudo apt install python3 && sudo apt install git && git clone https://github.com/xShamir/DDoS-Layer4 dos && cd dos && python3 DDoS.py
